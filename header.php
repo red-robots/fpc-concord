d38c6ee0bb7f6d604edcfa97b236cd6b852a366d
Austin Crane <austincrane@gmail.com>
2018-03-13 15:36:25 -0400

    add new fontawesome

--- header.php
+++ header.php
@@ -18,7 +18,8 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700|Ubuntu:400,700" rel="stylesheet">
 <link href="//cdnjs.cloudflare.com/ajax/libs/animate.css/3.0.0/animate.min.css" rel="stylesheet" type="text/css">
-<script src="https://use.fontawesome.com/762c66dd2b.js"></script>
+<!-- <script src="https://use.fontawesome.com/762c66dd2b.js"></script> -->
+<script src="https://use.fontawesome.com/8f931eabc1.js"></script>
 <?php 
 
 wp_head(); 
