c37ae48a42434c56c153a1ecc9df2aa0a53e2757
Austin Crane <austincrane@gmail.com>
2018-04-12 08:52:51 -0400

    add analytics

--- footer.php
+++ footer.php
@@ -60,6 +60,15 @@ if( !is_front_page() ) {
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
 <script src="//cdn.jsdelivr.net/stickynavbar.js/1.3.2/jquery.stickyNavbar.min.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-690301152-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </body>
 </html>
